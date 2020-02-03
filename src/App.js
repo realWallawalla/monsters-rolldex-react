fa25f4163c9aa76ff7a2776f2c283336c1d8bb90
Timon Sarakinis <timon.sarakinis@volvo.com>
2020-02-03 15:13:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './component/card-list/card-list.component';
+import { SearchBox } from './component/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    //this.handleChange = this.handleChange.bind(this);  bind sets the context of the handleChange function. Default behavouir in javascript is that fuctions does not set context.
+                                                      // Therefor by using bind(this). Binds the class app context to the handleChange function.
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value}) // arrow funtions automatically binds contexts where they are defined. dont have to bind context. called lexical scooping.
+  }
+  
+  render() {
+    const { monsters, searchField } = this.state;
+    //const monsters = this.state.monsters;
+    //const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+      //changing state in input causes render to be recalled. which will update filteredmonsters. 
+    return (
+      <div className="App">
+      <h1>Monsters RollerDex</h1>
+      <SearchBox 
+        placeholder='search monsters' 
+        handleChange={this.handleChange}>
+      </SearchBox>
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    )
+  }
 }
-
 export default App;
