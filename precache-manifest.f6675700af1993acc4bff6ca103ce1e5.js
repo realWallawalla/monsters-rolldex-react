0f8c7ea0725b7786f9284f4d26a5c5f92539c01e
Timon Sarakinis <timon.sarakinis@volvo.com>
2020-02-03 15:20:38 +0100

    Updates

---
+++ precache-manifest.f6675700af1993acc4bff6ca103ce1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d69c6397aa4c43900a36e7f981471f7",
+    "url": "/monsters-rolldex-react/index.html"
+  },
+  {
+    "revision": "76c0b99681b3e6f0f000",
+    "url": "/monsters-rolldex-react/static/css/main.87b67d16.chunk.css"
+  },
+  {
+    "revision": "5c20d58a3fb32156323c",
+    "url": "/monsters-rolldex-react/static/js/2.a8b34fc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolldex-react/static/js/2.a8b34fc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76c0b99681b3e6f0f000",
+    "url": "/monsters-rolldex-react/static/js/main.84bd68fc.chunk.js"
+  },
+  {
+    "revision": "ddb062f9b129544c35fd",
+    "url": "/monsters-rolldex-react/static/js/runtime-main.45d4566a.js"
+  }
+]);
